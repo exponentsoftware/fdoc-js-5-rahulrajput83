4e6a41b77e4e1645d764b432aefce575477f7d29
Rahul Rajput <rahulrajput21112@gmail.com>
2022-11-29 16:53:17 +0530

    Updated for largest country

--- Day.js
+++ Day.js
@@ -1,7 +1,8 @@
 import fetch from "node-fetch";
 
 const totalLanguages = [];
-
+let lang = [];
+let countryArea = []
 function fetchU() {
     
     fetch('https://restcountries.com/v3/all')
@@ -13,12 +14,32 @@ function fetchU() {
                         if(totalLanguages.indexOf(e) === -1) {
                             totalLanguages.push(e)
                         }
+                        let find = lang.findIndex((ele) => ele.language === e)
+                        
+                        if(find === -1) {
+                            lang.push({language: e, count: 1})
+                        }
+                        else {
+                            lang[find].count++;
+                        }
                     })
                     
                 }
-                
+                countryArea.push({country: res[i].name.common, area: res[i].area})
             }
             
+            lang = lang.sort((a, b) => {return b.count - a.count});
+            const topMost = [];
+            const area = []
+            for(let i = 0; i < 15; i++) {
+                topMost.push(lang[i])
+            }
+            countryArea = countryArea.sort((a, b) => b.area - a.area)
+            for(let i = 0; i < 10; i++) {
+                area.push(countryArea[i])
+            }
+            console.log(area)
+            console.log(topMost)
             console.log('Total Languages', totalLanguages.length);
         })
 }
